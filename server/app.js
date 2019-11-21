8a34d5d7af8382bd9e970e48f7903df200c4d71a
MimiMicek <makaveli_girl1996@hotmail.com>
2019-11-21 23:59:25 +0100

    added database

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const Knex = require('knex');
 const knexConfig = require('./knexfile');
@@ -7,6 +8,11 @@ const Model = require('objection').Model;
 
 const knex = Knex(knexConfig.development);
 Model.knex(knex);
+
+app.get("/backend", (req, res) => {
+    /* res.sendFile(path.join(__dirname + "/../client/public/index.html")); */
+    res.send({ express: 'The backend is now connected to React' });
+});
 
 const server = app.listen(8080, (err) => {
     if(err){
